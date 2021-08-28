2737eae2239a1a853a5962d6aa303b70bb93d8c9
Leonardo <leonardo@LAPTOP-2S3MJ3JD>
2021-08-28 17:31:28 -0300

    add model user.js

--- projeto/js/database/index.js
+++ projeto/js/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/utfpr');
+mongoose.connect('mongodb://localhost/utfpr',{useMongoClient: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
---
+++ projeto/js/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+name:{
+    type: String,
+    required: true,
+},
+email:{
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+},
+pwd:{
+    type: String,
+    required: true,
+    select: false,
+},    
+});
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = User;
